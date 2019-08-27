a5c7c14ce99d89d9426c7ce2f4bbc5af607df662
Francisco Javier Asensi Benito <asensi.benito@gmail.com>
2019-08-27 21:03:57 +0200

    Create PyCharm project and installed Django in Virtual Enviroment

---
+++ env/bin/python/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dynam1co/Proyectos_software/MacarronesConTomatico/env/bin/python/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
