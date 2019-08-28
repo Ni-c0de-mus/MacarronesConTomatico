f45f7ef937232d3c6f81f33fa1c10e9bd26fcdd2
Francisco Javier Asensi Benito <asensi.benito@gmail.com>
2019-08-28 21:39:24 +0200

    Exclude settings.py Connect to PostgreSQL Database

--- macarrones/settings.py
+++ macarrones/settings.py
@@ -72,14 +72,25 @@ WSGI_APPLICATION = 'macarrones.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'macarronestomatico',
+        'USER': 'macarron',
+        'PASSWORD': 'tomaticoMacarrones.?',
+        'HOST': 'localhost',
+        'PORT':'5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
