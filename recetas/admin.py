750e460004a11166733bf5a7a85dac6f2b5e65f3
Francisco Javier Asensi Benito <asensi.benito@gmail.com>
2019-08-29 14:14:51 +0200

    Register models in admin of app recetas

--- recetas/admin.py
+++ recetas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from recetas.models import *
 
-# Register your models here.
+admin.site.register(TipoReceta)
+admin.site.register(UnidadesMedida)
+admin.site.register(Categorias)
+admin.site.register(Receta)
+admin.site.register(RecetasCategorias)
+admin.site.register(Ingredientes)
+admin.site.register(PasosReceta)
+admin.site.register(ComentariosReceta)
+admin.site.register(ListaPersonalizadaUsuario)
+admin.site.register(DetalleListaUsuario)
