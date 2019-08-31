02032dd59e1f75fa78f27a906beb5b6559e8db94
Francisco Javier Asensi Benito <asensi.benito@gmail.com>
2019-08-31 11:46:29 +0200

    Responsive menu finished. Created all menu pages

--- recetas/urls.py
+++ recetas/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
-from recetas.views import HomeView
-
+from recetas.views import HomeView, UserRecetasView, CategoriasListView, TopRecetas, RecetasDeListView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     # Photo urls
     url(r'^$', HomeView.as_view(), name='recetas_home'),
+    url(r'^mis-recetas/$', login_required(UserRecetasView.as_view()), name='user_recetas'),
+    url(r'^categorias/$', CategoriasListView.as_view(), name='categorias_list'),
+    url(r'^top-recetas$', TopRecetas.as_view(), name='top_recetas'),
+    url(r'^recetas-de/$', RecetasDeListView.as_view(), name='recetas_de'),
 ]
--- recetas/views.py
+++ recetas/views.py
@@ -4,4 +4,64 @@ from django.views.generic import View, ListView
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'recetas/home.html')
+        context = {
+            'claseInicio': 'active',
+            'claseRecetasDe': '',
+            'claseTopRecetas': '',
+            'claseUserRecetas': '',
+            'claseCategorias_list': ''
+        }
+
+        return render(request, 'recetas/home.html', context)
+
+
+class CategoriasListView(View):
+    def get(self, request):
+        context = {
+            'claseInicio': '',
+            'claseRecetasDe': '',
+            'claseTopRecetas': '',
+            'claseUserRecetas': '',
+            'claseCategorias_list': 'active'
+        }
+
+        return render(request, 'recetas/categorias.html', context)
+
+
+class RecetasDeListView(View):
+    def get(self, request):
+        context = {
+            'claseInicio': '',
+            'claseRecetasDe': 'active',
+            'claseTopRecetas': '',
+            'claseUserRecetas': '',
+            'claseCategorias_list': ''
+        }
+
+        return render(request, 'recetas/recetasDe.html', context)
+
+
+class UserRecetasView(View):
+    def get(self, request):
+        context = {
+            'claseInicio': '',
+            'claseRecetasDe': '',
+            'claseTopRecetas': '',
+            'claseUserRecetas': 'active',
+            'claseCategorias_list': ''
+        }
+
+        return render(request, 'recetas/user_recetas.html', context)
+
+
+class TopRecetas(View):
+    def get(self, request):
+        context = {
+            'claseInicio': '',
+            'claseRecetasDe': '',
+            'claseTopRecetas': 'active',
+            'claseUserRecetas': '',
+            'claseCategorias_list': ''
+        }
+
+        return render(request, 'recetas/topRecetas.html', context)
