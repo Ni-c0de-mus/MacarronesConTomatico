ccc3434d1718be07bca0dcd674758544a3498933
Francisco Javier Asensi Benito <asensi.benito@gmail.com>
2019-09-02 20:37:20 +0200

    Show recipes in home page

--- recetas/views.py
+++ recetas/views.py
@@ -1,15 +1,26 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import View, ListView
 
+from recetas.models import Receta
 
-class HomeView(View):
+
+class RecetasQueryset(object):
+    def get_recetas_queryset(self, request):
+        recetas = Receta.objects.all()
+
+        return recetas
+
+
+class HomeView(View, RecetasQueryset):
     def get(self, request):
         context = {
             'claseInicio': 'active',
             'claseRecetasDe': '',
             'claseTopRecetas': '',
             'claseUserRecetas': '',
-            'claseCategorias_list': ''
+            'claseCategorias_list': '',
+            'recetas_list': self.get_recetas_queryset(request)
         }
 
         return render(request, 'recetas/home.html', context)
